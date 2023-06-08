a439421dcc22813003eeb498887f4716dc103558
dineshnikhil <dineshkumarpokkula@gmail.com>
2023-06-08 18:21:41 +0530

    done the the day2 problems

---
+++ Day2/p5.js
@@ -0,0 +1,17 @@
+// Write a function that takes an array as input and returns a new array with all duplicate elements removed.
+
+function remove_dup(nums) {
+	var num_dup = [];
+	num_dup.push(nums[0]);
+	for (let i = 1; i < nums.length; i++) {
+		for (let j = 0; j < num_dup.length; j++) {
+			if (nums[i] === num_dup[j]) {
+				console.log('true');
+			} else {
+				console.log('false');
+			}
+		}
+	}
+}
+
+remove_dup([2, 2, 3, 4, 3, 34]);
